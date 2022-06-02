b1baa249f3cf4dd53dbe050323c45c6311138d54
Mick McCarthy <mick@mccarthy.tech>
2022-06-02 12:15:35 +0100

    Create first car

--- Cars/CarTemplate.gd
+++ Cars/CarTemplate.gd
@@ -9,3 +9,16 @@ export var max_health: float = 100
 
 func _ready() -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	engine_force = 0
+	if Input.is_action_pressed("Player_1_Forward"):
+		if linear_velocity.length() < max_speed:
+			engine_force = throttle * delta
+	if Input.is_action_pressed("Player_1_Back"):
+		if linear_velocity.length() < max_speed:
+			engine_force = -throttle * delta
+
+	steering += (Input.get_action_strength("Player_1_Left") - Input.get_action_strength("Player_1_Right")) * delta
+	steering = clamp(steering, -max_steer, max_steer)
+	steering = lerp(steering, 0, 0.1)
