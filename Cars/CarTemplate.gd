ebecdfff38986b5f5d73c41cc080ede597295df7
Mick McCarthy <mick@mccarthy.tech>
2022-06-02 11:58:34 +0100

    Set up vehicle template

---
+++ Cars/CarTemplate.gd
@@ -0,0 +1,11 @@
+extends VehicleBody
+
+export var player_id: int = 0
+export var car_name: String = ''
+export var max_speed: float = 20
+export var throttle: float = 21000
+export var max_steer: float = 15
+export var max_health: float = 100
+
+func _ready() -> void:
+	pass
