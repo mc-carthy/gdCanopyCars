0f0e5eff2319c6cea1e6496d0a59655d4ba52fb9
Mick McCarthy <mick@mccarthy.tech>
2022-06-02 13:41:19 +0100

    Allow multiple controllable cars

--- Cars/CarTemplate.gd
+++ Cars/CarTemplate.gd
@@ -1,6 +1,6 @@
 extends VehicleBody
 
-export var player_id: int = 0
+export var player_id: int = 1
 export var car_name: String = ''
 export var max_speed: float = 20
 export var throttle: float = 210000
@@ -12,13 +12,13 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	engine_force = 0
-	if Input.is_action_pressed("Player_1_Forward"):
+	if Input.is_action_pressed("Player_" + str(player_id) + "_Forward"):
 		if linear_velocity.length() < max_speed:
 			engine_force = throttle * delta
-	if Input.is_action_pressed("Player_1_Back"):
+	if Input.is_action_pressed("Player_" + str(player_id) + "_Back"):
 		if linear_velocity.length() < max_speed:
 			engine_force = -throttle * delta
 
-	steering += (Input.get_action_strength("Player_1_Left") - Input.get_action_strength("Player_1_Right")) * delta
+	steering += (Input.get_action_strength("Player_" + str(player_id) + "_Left") - Input.get_action_strength("Player_" + str(player_id) + "_Right")) * delta
 	steering = clamp(steering, -max_steer, max_steer)
 	steering = lerp(steering, 0, 0.1)
